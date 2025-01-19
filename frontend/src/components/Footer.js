0909587051d1a048a8a4b9879c60966f60636400
User07Nidhi <roy.workspace76@gmail.com>
2025-01-19 21:46:54 +0530

    Commited

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,9 +11,16 @@ const Footer = () => {
                 </div>
                 <div className="footer-section">
                     <h4>Contact Us</h4>
-                    <p>Phone: +91 9876543210</p>
-                    <p>Email: info@bravitpharma.com</p>
-                    <p>Address: 123 Industrial Area, Vadodara, Gujarat, India</p>
+                    <p>Bravit Pharmaceuticals Private Limited
+                        506 Sakar East, Beside Gurunanak School, Tarsali, Vadodara 390009 Gujarat (India)</p>
+                    <p>Phone: 0265-2990343</p>
+
+                    <p>Email: bravitpharmaceuticals@gmail.com</p>
+
+                    <p>Time: Mon - Sat (10:00 - 19:00)</p>
+
+                    <p>To contact our customer support, please click here: <a>https://www.supportbravitpharmaceuticals.com</a></p>
+                    
                 </div>
                 <div className="footer-section">
                     <h4>Follow Us</h4>
@@ -26,7 +33,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-bottom">
-                <p>© 2024 Bravit Pharmaceuticals Pvt. Ltd. All rights reserved.</p> 
+                <p>© 2024 Bravit Pharmaceuticals Pvt. Ltd. All rights reserved.</p>
                 <a href="/privacy-policy">Privacy Policy | </a>
                 <a href="/terms-and-conditions">Terms and Conditions | </a>
                 <a href="/refund-policy">Refund Policy | </a>
